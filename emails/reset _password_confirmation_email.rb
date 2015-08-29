e099ccaf1856d88bbc7c05259c91e40d766881a3
Roman Parashchenko <romikoops1@gmail.com>
2015-08-29 23:16:12 +0200

    Update reset _password_confirmation_email.rb

--- emails/reset _password_confirmation_email.rb
+++ emails/reset _password_confirmation_email.rb
@@ -1,8 +1,13 @@
-class TestEmail < Email
-  SUBJECT = "Test email"
+class ResetPasswordConfirmationEmail < Email
+  SUBJECT = "Reset password instructions"
 
+  def confirm_my_account
+    WebPage.visit confirmation_link
+  end
 
-  def addressed_to?(new_user)
-    /Hi #{new_user}/ === plain_text_body
+  def confirmation_link
+    res = plain_text_body[/Change my password \((.+?)\)/, 1]
+    log.error("Confirmation link was not found") if res.nil?
+    res
   end
 end
