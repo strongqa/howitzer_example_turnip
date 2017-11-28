09ce7b1c49dfcb9654dc348bc95ec05ffa582302
lecik <lec1k2103@gmail.com>
2017-11-28 19:54:14 +0200

    feature changes

--- spec/steps/screenshot_steps.rb
+++ spec/steps/screenshot_steps.rb
@@ -5,7 +5,7 @@ module ScreenshotSteps
     HomePage.open
   end
 
-  step 'I force making screenshot' do
+  step 'I force making screenshot and saving source page' do
     HomePage.on do
       Capybara::Screenshot.screenshot_and_save_page
     end
@@ -13,9 +13,14 @@ module ScreenshotSteps
 
   # Then
 
-  step 'screenshot should be created and placed in log directory' do
+  step 'I should see screenshot in log directory' do
     screenshot = File.join(File.dirname(__FILE__), '../../log/screenshot.png')
-    expect(File.exist?(screenshot)).to be_truthy
+    expect(File).to be_exist(screenshot)
+  end
+
+  step 'I should see source page in log directory' do
+    source_page = File.join(File.dirname(__FILE__), '../../log/screenshot.html')
+    expect(File).to be_exist(source_page)
   end
 end
 
