99810564d8b4e805b0fa4bbab9bba491a649a562
Yevhen Plakhotnikov <yevhen@strongs-Mac-mini.local>
2019-08-15 18:43:30 +0300

    Added new tests

--- spec/acceptance/articles/articles_list.feature
+++ spec/acceptance/articles/articles_list.feature
@@ -10,3 +10,22 @@ Feature: Article list
     And there is an article
     When I open articles list page
     Then I should see article in articles list page with correct article parameters
+
+  Scenario: user can find necessary articles by name in Article list
+    Given there is user in the system
+    And I am logged to the system as user
+    And there is an article
+    When I open articles list page
+    And I search necessary article in right sidebar on article list page
+    Then I should be redirected to search page
+    And I should see article on search page
+
+  Scenario: user can see and open recently created articles in right sidebar of Article list
+    Given there is user in the system
+    And I am logged to the system as user
+    And there is an article
+    When I open articles list page
+    Then I should see created article in recent post on article list page
+    When I click on article in recent post on article list page
+    Then I should be redirected to article page
+    And I should see article parameters on article page
--- spec/steps/articles/articles_list_steps.rb
+++ spec/steps/articles/articles_list_steps.rb
@@ -3,11 +3,39 @@ module ArticlesListSteps
 
   # WHEN
 
+  step 'I click on article in recent post on article list page' do
+    s = self
+    ArticleListPage.on { open_recent_post(s.article.title) }
+  end
+
   # THEN
 
   step 'I should see article in articles list page with correct article parameters' do
     s = self
     ArticleListPage.on do
+      expect(text).to include(s.article.title.upcase)
+      expect(text).to include(s.article.text)
+    end
+  end
+
+  step 'I search necessary article in right sidebar on article list page' do
+    s = self
+    ArticleListPage.on { search_article(s.article.title) }
+  end
+
+  step 'I should see article on search page' do
+    s = self
+    SearchPage.on { is_expected.to have_article_element(s.article.title) }
+  end
+
+  step 'I should see created article in recent post on article list page' do
+    s = self
+    ArticleListPage.on { is_expected.to have_recent_post_element(s.article.title) }
+  end
+
+  step 'I should see article parameters on article page' do
+    s = self
+    ArticlePage.on do
       expect(text).to include(s.article.title.upcase)
       expect(text).to include(s.article.text)
     end
