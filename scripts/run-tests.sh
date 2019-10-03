cbd45e0aa8de5137b8b81b72997e9b3df04ad00c
Vitaliy Franchuk <32457654+fravit4@users.noreply.github.com>
2019-10-03 23:16:22 +0300

    add trigger to travis (#32)
    * add trigger to travis
    
    * add trigger to travis 1
    
    * check other repos

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 set -ev
+
+CUCUMBER_STATE=("started")
+RSPEC_STATE=("started")
+while [[ " ${CUCUMBER_STATE[*]} " == *"started"* ]] || [[ " ${RSPEC_STATE[*]} " == *"started"* ]]; do
+        sleep 5
+        CUCUMBER_STATE=$(curl -s -X GET \
+       -H "Content-Type: application/json" \
+       -H "Accept: application/json" \
+       -H "Travis-API-Version: 3" \
+       -H "Authorization: token ${QA_TOKEN}" \
+       https://api.travis-ci.org/repo/${CUCUMBER_SLUG}/builds | jq -r '[.builds[].state]' )
+        RSPEC_STATE=$(curl -s -X GET \
+       -H "Content-Type: application/json" \
+       -H "Accept: application/json" \
+       -H "Travis-API-Version: 3" \
+       -H "Authorization: token ${QA_TOKEN}" \
+       https://api.travis-ci.org/repo/${RSPEC_SLUG}/builds | jq -r '[.builds[].state]' )
+done
+
 if [[ "$SEXY_SETTINGS" =~ .*headless_firefox.* ]]
 then
     wget https://github.com/mozilla/geckodriver/releases/download/v0.25.0/geckodriver-v0.25.0-linux64.tar.gz
